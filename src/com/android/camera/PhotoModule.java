f614d10b8cf2f4ea68fb1547dc9efb1b767db9bf
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-12-25 13:50:51 +0700

    Camera2: Preview needs to be stopped when changing resolution
    This was done in startPreview as a hack, but was removed in
    f729b85e12cfa2674048d8cce2f724014b8fc910. Specifically add it
    back when the resolution is changed.
    
    Change-Id: I306abd788bafdff4e6fba7eec4e6ff05bb3cc47c

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1550,6 +1550,12 @@ public class PhotoModule
             stopPreview();
         }
 
+        // Preview needs to be stopped when changing resolution
+        if (mRestartPreview && mCameraState != PREVIEW_STOPPED) {
+            stopPreview();
+            mRestartPreview = false;
+        }
+
         setDisplayOrientation();
 
         if (!mSnapshotOnIdle && !mAspectRatioChanged) {
